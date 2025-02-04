4ff3948b24d1dad49da9903694238ca2c81450d0
Krystian Tyśkiewicz <krystian.tyskiewicz@gmail.com>
2025-02-04 18:14:31 +0100

    Blink, LoveOMeter, Spaceship

---
+++ ArduinoUno/LoveOMeter/LoveOMeter.ino
@@ -0,0 +1,49 @@
+const int sensorPin = A0;
+// room temperature in Celsius
+const float baselineTemp = 20.0;
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
+}
+
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("sensor Value: ");
+  Serial.print(sensorVal);
+
+  float voltage = (sensorVal / 1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  float temperature = voltage * 100;
+  Serial.print(", degrees C: ");
+  Serial.println(temperature);
+
+  // if the current temperature is lower than the baseline turn off all LEDs
+  if (temperature < baselineTemp + 2) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }  // if the temperature rises 2-4 degrees, turn an LED on
+  else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }  // if the temperature rises 4-6 degrees, turn a second LED on
+  else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }  // if the temperature rises more than 6 degrees, turn all LEDs on
+  else if (temperature >= baselineTemp + 6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
+}
