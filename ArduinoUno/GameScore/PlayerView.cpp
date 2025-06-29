2439037b0083b7c09f655913796de1565a302f5d
Krystian Tyśkiewicz <krystian.tyskiewicz@gmail.com>
2025-06-29 18:05:11 +0200

    GameScore - add more options for points calculating

--- ArduinoUno/GameScore/PlayerView.cpp
+++ ArduinoUno/GameScore/PlayerView.cpp
@@ -20,12 +20,36 @@ void PlayerView::draw() {
     tft.setCursor(10, 100);
     tft.print("Dodaj");
 
+    tft.setTextColor(BLACK);
+    tft.setCursor(10, 190);
+    tft.print("Odejmij");
+
+    tft.setTextColor(BLACK);
+    tft.setCursor(10, 220);
+    tft.print("Ustaw");
+
     numberKeyboard.draw();
 }
 
 void PlayerView::handleScreenPoint(ScreenPoint screenPoint) {
     if (screenPoint.x > 10 && screenPoint.x < 70 && screenPoint.y > 100 && screenPoint.y < 124) {
         game.getCurrentPlayer().points += numberKeyboard.getTypedText().toInt();
+        numberKeyboard.clearTypedText();
+        game.goToGameView();
+
+        return;
+    }
+
+    if (screenPoint.x > 10 && screenPoint.x < 84 && screenPoint.y > 190 && screenPoint.y < 214) {
+        game.getCurrentPlayer().points -= numberKeyboard.getTypedText().toInt();
+        numberKeyboard.clearTypedText();
+        game.goToGameView();
+
+        return;
+    }
+
+    if (screenPoint.x > 10 && screenPoint.x < 70 && screenPoint.y > 220 && screenPoint.y < 244) {
+        game.getCurrentPlayer().points = numberKeyboard.getTypedText().toInt();
         numberKeyboard.clearTypedText();
         game.goToGameView();
 
